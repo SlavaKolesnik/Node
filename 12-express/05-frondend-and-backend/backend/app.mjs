cf69fcb7601a37b88128f9a46ccd89639c4ac661
Slava Kolesnik <slava1995kolesnik@gmail.com>
2024-09-19 22:25:07 +0300

    front+back

--- 12-express/05-frondend-and-backend/backend/app.mjs
+++ 12-express/05-frondend-and-backend/backend/app.mjs
@@ -1,5 +1,6 @@
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 
 const app = express()
 
@@ -9,10 +10,16 @@ app.use(morgan('tiny'))
 app.use(express.json())
 // converts from data to JS Object in POST, PUT, PATCH request
 app.use(express.urlencoded({ extended: true }))
+// enable all CORS requests
+app.use(cors())
 
 app.use((req, res) => {
+    const personData = {
+        name: 'Slava',
+        isInstructor: true,
+    }
     console.log(req.body)
-    return res.send('This is express server')
+    return res.json(personData)
 })
 
 app.listen(5000, () => {
